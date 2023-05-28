1217cf77e582cb8863d9701ba1a251eeae114ac4
Diego Mercado <diego.mercado1997@hotmail.com>
2023-05-28 19:51:35 -0300

    dando margin a la caja del color

--- src/components/ItemColores.jsx
+++ src/components/ItemColores.jsx
@@ -3,11 +3,11 @@ import { Button, Card } from 'react-bootstrap';
 
 const ItemColores = ({color}) => {
     return (
-       <Card className='col-12 col-lg-3 col-sm-3 mt-3'>
+       <Card className='col-12 col-lg-3 col-sm-3 mt-3 shadow'>
         <Card.Header className='text-center'>{color.toString().toUpperCase()}</Card.Header>
-        <Card.Body className='mColor' style={{backgroundColor:color,width:"80px",height:"80px"}}></Card.Body>
+        <Card.Body className='mColor my-3' style={{backgroundColor:color,width:"80px",height:"80px"}}></Card.Body>
         <Card.Footer className='d-flex justify-content-end'>
-            <Button type='button' variant='danger' className='ms-auto'>Borrar</Button>
+            <Button type='button' variant='danger' className='ms-auto shadow'>Borrar</Button>
             </Card.Footer>
        </Card>
     );
